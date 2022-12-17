6cae571287eaeb4a81f5d34098cfa88560782945
Siriwat Huntra <siriwat26room1@gmail.com>
2022-12-17 16:16:34 +0700

    .1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,7 @@
-# When done, submit this entire file to the autograder.
+/part 1/
+/arr.sum/
+30.times print “Ruby!”
 
-# Part 1
 
-def sum arr
-  puts "hello word"
-end
 
-def max_2_sum arr
-  # YOUR CODE HERE
-end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
-end
-
-# Part 2
-
-def hello(name)
-  # YOUR CODE HERE
-end
-
-def starts_with_consonant? s
-  # YOUR CODE HERE
-end
-
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
-end
-
-# Part 3
-
-class BookInStock
-# YOUR CODE HERE
-end
