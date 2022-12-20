7f4a702cc6e75e7454698b999c62d36972fefe37
Siriwat Huntra <siriwat26room1@gmail.com>
2022-12-20 18:47:54 +0700

    port file

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,9 +1,49 @@
-puts "hello world"
+# When done, submit this entire file to the autograder.
 
-def sum(array)
-    array.each {|i| sum += i}
+# Part 1
+
+def sum arr
+    arr.sum
 end
 
-def max_2_sum(array)
-    puts array.max(2).sum
+def max_2_sum arr
+    a = arr.max(2){ |a, b| a<=>b }
+    a = a.sum
+end
+
+def sum_to_n? arr, n
+    
+    if arr.length == 0
+        return false
+    elsif arr.length == 1
+        return false
+    end
+    arr.each_with_index do |x, index|
+        arr.drop(index).each_with_index do |y, index2|
+            if (x + y) == n and index != index2 + index
+                return true
+            end
+        end
+    end
+    return false
+end
+
+# Part 2
+
+def hello(name)
+    return "Hello, #{name}"
+end
+
+def starts_with_consonant? s
+  # YOUR CODE HERE
+end
+
+def binary_multiple_of_4? s
+  # YOUR CODE HERE
+end
+
+# Part 3
+
+class BookInStock
+# YOUR CODE HERE
 end
