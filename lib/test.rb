9c8fd9c09efd77949bb3228d8f977a2244c4164f
Siriwat Huntra <siriwat26room1@gmail.com>
2022-12-20 21:09:41 +0700

    .

--- lib/test.rb
+++ lib/test.rb
@@ -1,7 +1,6 @@
 def vowelOrConsonant(x)
   consonant_array = "b c d f g h j k l m n p q r s t v x y z".split
-  puts consonant_array
-  if (consonant_array.include? x[0,1])
+  if (consonant_array.include? x)
       print("consonant \n")
   else
       print("it not consonant \n")
@@ -10,3 +9,4 @@ end
 
 vowelOrConsonant('c')
 vowelOrConsonant('E')
+vowelOrConsonant(555)
