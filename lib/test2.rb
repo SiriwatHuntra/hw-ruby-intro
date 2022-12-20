5328aa0beea651840f6351cf51c56daa2544c1f1
Siriwat Huntra <siriwat26room1@gmail.com>
2022-12-20 21:09:35 +0700

    .

--- lib/test2.rb
+++ lib/test2.rb
@@ -1,4 +1,13 @@
-s = "Hello"
-s = s.downcase
-puts s[0,1]
+def binary_multiple_of_4? s
+  if s.is_a? String && s.chr.each {|x| x == "1" || x == "0" }
+    puts "Program is runed"
+
+  else
+    puts "Your input isn't Integer"
+  end
+end
+
+binary_multiple_of_4?(5)
+binary_multiple_of_4?("10101")
+binary_multiple_of_4?("555")
 
