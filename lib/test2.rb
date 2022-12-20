6a6fea9284c13cd611a58b87bbb2e2d577a89fad
Siriwat Huntra <siriwat26room1@gmail.com>
2022-12-20 21:38:44 +0700

    .

--- lib/test2.rb
+++ lib/test2.rb
@@ -1,20 +1,16 @@
 def binary_multiple_of_4? s
   if s.is_a? String
-    puts "Program is runed"
     if s.delete('01') == ''
-      puts "Input is binary"
-      s = s.to_i(2)
-      puts s
-      return
+      puts 0 == (s.to_i(2))%4
     else
-      puts "You input isn't binary"
+      return false
     end
   else
-    puts "Your input isn't String"
+    return false
   end
 end
 
 binary_multiple_of_4?(5)
 binary_multiple_of_4?("101")
-binary_multiple_of_4?("555")
+binary_multiple_of_4?("100")
 
